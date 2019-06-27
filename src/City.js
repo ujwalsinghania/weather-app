54e430f3b28f46fb5db0451c24e82f18abbdd9e6
ujwal <ujwal.singhania@hotmail.com>
2019-06-27 16:15:08 +0530

    weather app

--- src/City.js
+++ src/City.js
@@ -21,7 +21,7 @@ class City extends Component{
 
     render() {
        return (
-           <Container>
+           <Container style={{"margin-top":"20px"}}>
                <form onSubmit={this.sendCity}>
                    <input type={"text"} name={"city"} onChange={this.handleChange} className={"input-style"} placeholder={"Enter a City"}/>
                    <input type="submit" className={"btn btn-primary"}/>
